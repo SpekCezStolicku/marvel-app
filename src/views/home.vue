7d9c96b931bb5ebf6e245737a2203e1bb51633c8
Speky <martin.mozucha@gmail.com>
2022-02-07 17:11:30 +0100

    create home views layout

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <SearchBar />
+  </div>
+</template>
+
+<script>
+import SearchBar from "../components/SearchBar.vue";
+export default {
+  components: {
+    SearchBar,
+  },
+};
+</script>
