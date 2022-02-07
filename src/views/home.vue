e5123c22dc2fad8e524eb8c08bbdbeca087192d9
Speky <martin.mozucha@gmail.com>
2022-02-07 17:20:44 +0100

    import HeroCard to home view file

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container">
     <SearchBar />
+    <HeroCard />
   </div>
 </template>
 
 <script>
 import SearchBar from "../components/SearchBar.vue";
+import HeroCard from "../components/reusable/HeroCard.vue";
 export default {
   components: {
     SearchBar,
+    HeroCard,
   },
 };
 </script>
