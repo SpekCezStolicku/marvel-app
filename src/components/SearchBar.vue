02beb91d70a24173266a57255811c8f98a120a99
Speky <martin.mozucha@gmail.com>
2022-02-07 23:31:56 +0100

    added styles in SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -20,17 +20,24 @@
 }
 input {
   width: 35%;
-  height: 3.5em;
-  border: 0;
+  font-size: 1.1em;
+  height: 3em;
+  border: none;
+  box-shadow: 1px 1px 8px black;
+  padding-left: 2em;
   border-top-left-radius: var(--b-radius);
   border-bottom-left-radius: var(--b-radius);
   background: var(--snow);
 }
+input:focus {
+  outline: none;
+}
 button {
   padding: 0 1em;
   background: var(--snow);
-  border: 0;
+  border: none;
   border-top-right-radius: var(--b-radius);
   border-bottom-right-radius: var(--b-radius);
+  cursor: pointer;
 }
 </style>
