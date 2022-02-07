110de33ed8db0d8fa002e5fa6ed0b5bd2687a3fe
Speky <martin.mozucha@gmail.com>
2022-02-07 15:52:05 +0100

    create layout in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
+<script setup>
+import NavBar from "./components/NavBar.vue";
+</script>
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <NavBar />
+  <router-view></router-view>
 </template>
 
 <style>
+@import "./assets/css/global.css";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  min-width: 100vw;
+  min-height: 100vh;
+  font-family: "Roboto-Condensed-Regular", Helvetica, Arial;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  background: var(--dark);
+  color: var(--snow);
 }
 </style>
