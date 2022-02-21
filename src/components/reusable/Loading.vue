427fed0ecc2e8328a3d0fb30b8aff6d8bf76acb2
Speky <martin.mozucha@gmail.com>
2022-02-21 10:03:26 +0100

    add style to Loading component

--- src/components/reusable/Loading.vue
+++ src/components/reusable/Loading.vue
@@ -3,3 +3,14 @@
     <img src="../../assets/loading.gif" alt="loading" />
   </div>
 </template>
+
+<style scoped>
+div {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+}
+</style>
